eb07d4d7a71ec3ed0ada24f0619347f2a352c4c1
Atulananda Panigrahy <atula.panigrahy@gmail.com>
2023-01-01 13:59:49 +0530

    Create real-time sender and receiver pages
    files created:
    - public/index.html
    - public/real-time-style.css
    - public/receiver.html
    - public/receiver.js
    - public/sender.js

---
+++ public/receiver.js
@@ -0,0 +1,73 @@
+function init() {
+  let senderID;
+  const socket = io();
+  // let io = require("socket.io-client");
+  // let socket = Io.connect("http://localhost:3000/", {
+  //   reconnection: true,
+  // });
+
+  const generateID = () => {
+    return `${Math.trunc(Math.random() * 999)}-${Math.trunc(
+      Math.random() * 999
+    )}-${Math.trunc(Math.random() * 999)}`;
+  };
+
+  document
+    .querySelector("#receiver-start-con-btn")
+    .addEventListener("click", (event) => {
+      senderID = document.querySelector("#join-id").value;
+      if (senderID.length === 0) {
+        return;
+      }
+
+      let joinID = generateID();
+      console.log(joinID);
+      document.querySelector("#join-id").innerHTML = `
+      <b>Room ID</b>
+      <span>${joinID}</span>`;
+
+      socket.emit("receiver-join", { uid: joinID, sender_uid: senderID });
+      document.querySelector(".join-screen").classList.remove("active");
+      document.querySelector(".fs-screen").classList.add("active");
+    });
+
+  let fileShare = {};
+  socket.on("fs-meta", (metadata) => {
+    fileShare.metadata = metadata;
+    fileShare.transmitted = 0;
+    fileShare.buffer = [];
+
+    let el = document.createElement("div");
+    el.classList.add("item");
+    el.innerHTML = `
+      <div class="progress">0%</div>
+      <div class="filename">${metadata.filename}</div>
+      `;
+    document.querySelector(".files-list").appendChild(el);
+
+    fileShare.progress_node = el.querySelector(".progress");
+    socket.emit("fs-start", {
+      uid: senderID,
+    });
+  });
+
+  socket.on("fs-share", function (buffer) {
+    fileShare.buffer.push(buffer);
+    fileShare.transmitted += buffer.byteLength;
+    fileShare.progress_node.innerText =
+      Math.trunc(
+        (fileShare.transmitted / fileShare.metadata.total_buffer_size) * 100
+      ) + "%";
+
+    if (fileShare.transmitted === fileShare.metadata.total_buffer_size) {
+      download(new Blob(fileShare.buffer), fileShare.metadata.filename);
+      fileShare = {};
+    } else {
+      socket.emit("fs-start", {
+        uid: senderID,
+      });
+    }
+  });
+}
+
+init();
